1236c7d756cda16067c1297409e8d6b7228d7a97
Boran Yildirim <boranyldrm21@gmail.com>
2020-10-28 15:18:53 +0300

    kafkaURL

--- poller/main.go
+++ poller/main.go
@@ -14,7 +14,7 @@ import (
 
 func main() {
 	// get kafka writer using environment variables.
-	kafkaURL := os.Getenv("kafkaURL")
+	kafkaURL := "kafka:9092"
 	consumeTopic := "watch"
 	consumeGroup := "watch-group"
 
